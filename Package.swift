08e3ae8fd7d0aa92c45658908419a5fb9f0f0dd2
Mattt <mattt@me.com>
2020-01-24 02:39:21 -0800

    Add CommonMarkBuilder to products

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "CommonMark",
-            targets: ["CommonMark"]
+            targets: ["CommonMark", "CommonMarkBuilder"]
         ),
     ],
     dependencies: [
