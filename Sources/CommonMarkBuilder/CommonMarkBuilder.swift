ffe900bf292680fd352a336d110d8c57f54ff2b9
Mattt <mattt@me.com>
2020-01-25 06:47:31 -0800

    Conform inline types to ListItemConvertible

--- Sources/CommonMarkBuilder/CommonMarkBuilder.swift
+++ Sources/CommonMarkBuilder/CommonMarkBuilder.swift
@@ -91,7 +91,25 @@ public protocol ListItemConvertible {
     var listItemValue: [List.Item] { get }
 }
 
+// MARK: Default Implementations
+
+extension Inline where Self: Node {
+    public var listItemValue: [List.Item] {
+        return [List.Item(children: [self])]
+    }
+}
+
 // MARK: Conformances
+
+extension Text: ListItemConvertible {}
+extension Strong: ListItemConvertible {}
+extension Emphasis: ListItemConvertible {}
+extension Link: ListItemConvertible {}
+extension Image: ListItemConvertible {}
+extension Code: ListItemConvertible {}
+extension HTML: ListItemConvertible {}
+extension SoftLineBreak: ListItemConvertible {}
+extension HardLineBreak: ListItemConvertible {}
 
 extension List.Item: ListItemConvertible {
     public var listItemValue: [List.Item] {
