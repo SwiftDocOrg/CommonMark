fbb269cb1d048e6e1ddce399d61f9aa140a876ef
Mattt <mattt@me.com>
2020-02-06 14:53:44 -0800

    Run tests with --enable-test-discovery option
    Remove unnecessary XCTestManifests

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,12 +1,1 @@
-import XCTest
-
-import CommonMarkBuilderTests
-import CommonMarkSpecTests
-import CommonMarkTests
-
-var tests = [XCTestCaseEntry]()
-tests += CommonMarkBuilderTests.__allTests()
-tests += CommonMarkSpecTests.__allTests()
-tests += CommonMarkTests.__allTests()
-
-XCTMain(tests)
+fatalError("Run with `swift test --enable-test-discovery`")
