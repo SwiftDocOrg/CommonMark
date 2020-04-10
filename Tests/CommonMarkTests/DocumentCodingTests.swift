727e38f448dfc5ee8d1cd144bea859d35114b629
Mattt <mattt@me.com>
2020-04-10 13:47:39 -0700

    Fix 'Top-level Document encoded as string JSON fragment.'

--- Tests/CommonMarkTests/DocumentCodingTests.swift
+++ Tests/CommonMarkTests/DocumentCodingTests.swift
@@ -5,14 +5,12 @@ final class DocumentCodingTests: XCTestCase {
     func testDocumentCodingRoundTrip() throws {
         let document = try Document(Fixtures.uhdr)
 
-        let encoder = JSONEncoder()
-        let encoded = try encoder.encode(document)
-
-        let decoder = JSONDecoder()
-        let decoded = try decoder.decode(Document.self, from: encoded)
+        // Workaround for "Top-level Document encoded as string JSON fragment."
+        let encoded = try JSONEncoder().encode([document])
+        let decoded = try JSONDecoder().decode([Document].self, from: encoded)
 
         let expected = document.render(format: .html)
-        let actual = decoded.render(format: .html)
+        let actual = decoded[0].render(format: .html)
 
         XCTAssertEqual(expected, actual)
     }
@@ -22,14 +20,12 @@ final class DocumentCodingTests: XCTestCase {
 
         let heading = document.children.first! as! Heading
 
-        let encoder = JSONEncoder()
-        let encoded = try encoder.encode(heading)
-
-        let decoder = JSONDecoder()
-        let decoded = try decoder.decode(Heading.self, from: encoded)
+        // Workaround for "Top-level Heading encoded as string JSON fragment."
+        let encoded = try JSONEncoder().encode([heading])
+        let decoded = try JSONDecoder().decode([Heading].self, from: encoded)
 
         let expected = try Document(heading.description).render(format: .html)
-        let actual = try Document(decoded.description).render(format: .html)
+        let actual = try Document(decoded[0].description).render(format: .html)
 
         XCTAssertEqual(expected, actual)
     }
@@ -40,14 +36,12 @@ final class DocumentCodingTests: XCTestCase {
         let heading = document.children.first! as! Heading
         let link = heading.children.first! as! Link
 
-        let encoder = JSONEncoder()
-        let encoded = try encoder.encode(link)
-
-        let decoder = JSONDecoder()
-        let decoded = try decoder.decode(Link.self, from: encoded)
+        // Workaround for "Top-level Link encoded as string JSON fragment."
+        let encoded = try JSONEncoder().encode([link])
+        let decoded = try JSONDecoder().decode([Link].self, from: encoded)
 
         let expected = try Document(link.description).render(format: .html)
-        let actual = try Document(decoded.description).render(format: .html)
+        let actual = try Document(decoded[0].description).render(format: .html)
 
         XCTAssertEqual(expected, actual)
     }
