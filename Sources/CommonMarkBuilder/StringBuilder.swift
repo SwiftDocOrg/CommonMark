34cfac47577471ad9824f14a0df18f5c67b512b6
Mattt <mattt@me.com>
2020-04-10 12:43:18 -0700

    Implement buildIf and buildEither for StringBuilder

--- Sources/CommonMarkBuilder/StringBuilder.swift
+++ Sources/CommonMarkBuilder/StringBuilder.swift
@@ -1,10 +1,28 @@
 @_functionBuilder
 public struct StringBuilder {
+    // MARK: buildBlock
+
     public static func buildBlock(_ string: String) -> String {
         return string
     }
 
     public static func buildBlock(_ strings: String...) -> String {
         return strings.joined(separator: "\n\n")
+    }
+
+    // MARK: buildIf
+
+    public static func buildIf(_ string: String?) -> String {
+        return string ?? ""
+    }
+
+    // MARK: buildEither
+
+    public static func buildEither(first: String) -> String {
+        return first
+    }
+
+    public static func buildEither(second: String) -> String {
+        return second
     }
 }
