729c1f3b8b9a70278eb6c6e4d5c7b4f795a25b87
Lukas Stührk <Lukas@Stuehrk.net>
2020-04-23 22:12:10 +0200

    Fix failing container of blocks manipulation.

--- Sources/CommonMark/Supporting Types/Children.swift
+++ Sources/CommonMark/Supporting Types/Children.swift
@@ -109,7 +109,7 @@ extension ContainerOfBlocks {
     */
     @discardableResult
     public func insert(child: Block & Node, before sibling: Block & Node) -> Bool {
-        return add(child) { cmark_node_insert_before(child.cmark_node, sibling.cmark_node) }
+        return add(child) { cmark_node_insert_before(sibling.cmark_node, child.cmark_node) }
     }
 
     /**
@@ -122,7 +122,7 @@ extension ContainerOfBlocks {
     */
     @discardableResult
     public func insert(child: Block & Node, after sibling: Block & Node) -> Bool {
-        return add(child) { cmark_node_insert_after(child.cmark_node, sibling.cmark_node) }
+        return add(child) { cmark_node_insert_after(sibling.cmark_node, child.cmark_node) }
     }
 
     /**
