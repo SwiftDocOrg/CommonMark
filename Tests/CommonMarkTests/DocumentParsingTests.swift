3ec3341edb990f092b6471c66689cb6acccf4006
Mattt <mattt@me.com>
2020-04-23 06:01:22 -0700

    Add assertions that make testDocumentParsing fail

--- Tests/CommonMarkTests/DocumentParsingTests.swift
+++ Tests/CommonMarkTests/DocumentParsingTests.swift
@@ -10,17 +10,21 @@ final class DocumentParsingTests: XCTestCase {
         let heading = document.children[0] as! Heading
         XCTAssertEqual(heading.level, 1)
         XCTAssertEqual(heading.children.count, 1)
+        XCTAssertEqual(heading.parent, document)
 
         let link = heading.children[0] as! Link
         XCTAssertEqual(link.urlString, "https://www.un.org/en/universal-declaration-human-rights/")
         XCTAssertEqual(link.title, "View full version")
+        XCTAssertEqual(link.parent, heading)
 
         let subheading = document.children[1] as! Heading
         XCTAssertEqual(subheading.level, 2)
         XCTAssertEqual(subheading.children.count, 1)
+        XCTAssertEqual(subheading.parent, document)
 
         let subheadingText = subheading.children[0] as! Text
         XCTAssertEqual(subheadingText.literal, "Article 1.")
+        XCTAssertEqual(subheadingText.parent, subheading)
 
         let paragraph = document.children[2] as! Paragraph
         XCTAssert(paragraph.description.starts(with: "All human beings"))
@@ -28,6 +32,7 @@ final class DocumentParsingTests: XCTestCase {
         XCTAssertEqual(paragraph.range.lowerBound.column, 1)
         XCTAssertEqual(paragraph.range.upperBound.line, 7)
         XCTAssertEqual(paragraph.range.upperBound.column, 62)
+        XCTAssertEqual(paragraph.parent, document)
     }
 
     // https://github.com/SwiftDocOrg/CommonMark/issues/1
