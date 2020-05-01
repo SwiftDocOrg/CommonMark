a62648054393775dcaa58b2693aafbb0299be644
Mattt <mattt@me.com>
2020-05-01 12:59:53 -0700

    Move render method from Document to Node

--- Sources/CommonMark/Nodes/Node.swift
+++ Sources/CommonMark/Nodes/Node.swift
@@ -107,6 +107,107 @@ public class Node: Codable {
         return Node.create(for: cmark_node_parent(cmark_node))
     }
 
+    // MARK: - Rendering
+
+    /// Formats for rendering a document.
+    public enum RenderingFormat {
+        /// CommonMark
+        case commonmark
+
+        /// HTML
+        case html
+
+        /// XML
+        case xml
+
+        /// LaTeX
+        case latex
+
+        /// Manpage
+        case manpage
+    }
+
+    /// Options for rendering a CommonMark document.
+    public struct RenderingOptions: OptionSet {
+        public var rawValue: Int32
+
+        public init(rawValue: Int32 = CMARK_OPT_DEFAULT) {
+            self.rawValue = rawValue
+        }
+
+        /**
+         Render raw HTML and "unsafe" links.
+
+         A link is considered to be "unsafe"
+         if its scheme is `javascript:`, `vbscript:`, or `file:`,
+         or if its scheme is `data:`
+         and the MIME type of the encoded data isn't one of the following:
+
+         - `image/png`
+         - `image/gif`
+         - `image/jpeg`
+         - `image/webp`
+
+         By default,
+         raw HTML is replaced by a placeholder HTML comment.
+         Unsafe links are replaced by empty strings.
+
+         - Important: This option has an effect only when rendering HTML.
+         */
+        public static let unsafe = RenderingOptions(rawValue: CMARK_OPT_UNSAFE)
+
+        /**
+         Render softbreak elements as spaces.
+
+         - Important: This option has no effect when rendering XML.
+         */
+        public static let noBreaks = RenderingOptions(rawValue: CMARK_OPT_NOBREAKS)
+
+        /**
+         Render softbreak elements as hard line breaks.
+
+         - Important: This option has no effect when rendering XML.
+         */
+        public static let hardBreaks = RenderingOptions(rawValue: CMARK_OPT_HARDBREAKS)
+
+        /**
+         Include a `data-sourcepos` attribute on all block elements
+         to map the rendered output to the source input.
+
+         - Important: This option has an effect only when rendering HTML or XML.
+         */
+        public static let includeSourcePosition = RenderingOptions(rawValue: CMARK_OPT_SOURCEPOS)
+    }
+
+    /**
+     Render a document into a given format with the specified options.
+
+     - Parameters:
+        - format: The rendering format
+        - options: The rendering options
+        - width: The column width used to wrap lines for rendered output
+                 (`.commonmark`, `.man`, and `.latex` formats only).
+                 Must be a positive number.
+                 Pass `0` to prevent line wrapping.
+     - Returns: The rendered text.
+     */
+    public func render(format: RenderingFormat, options: RenderingOptions = [], width: Int = 0) -> String {
+        precondition(width >= 0)
+
+        switch format {
+        case .commonmark:
+            return String(cString: cmark_render_commonmark(cmark_node, options.rawValue, Int32(clamping: width)))
+        case .html:
+            return String(cString: cmark_render_html(cmark_node, options.rawValue))
+        case .xml:
+            return String(cString: cmark_render_xml(cmark_node, options.rawValue))
+        case .latex:
+            return String(cString: cmark_render_latex(cmark_node, options.rawValue, Int32(clamping: width)))
+        case .manpage:
+            return String(cString: cmark_render_man(cmark_node, options.rawValue, Int32(clamping: width)))
+        }
+    }
+
     // MARK: - Codable
 
     public required convenience init(from decoder: Decoder) throws {
