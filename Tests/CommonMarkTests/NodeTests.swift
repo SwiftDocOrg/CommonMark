ba00ca94d0f512a5d986fd69cc4aef5dd192e795
Mattt <mattt@me.com>
2021-03-03 11:22:47 -0800

    Refactor testNodesEquality to use interpolated constant for common text

--- Tests/CommonMarkTests/NodeTests.swift
+++ Tests/CommonMarkTests/NodeTests.swift
@@ -6,16 +6,14 @@ final class NodeTests: XCTestCase {
     // MARK: - Equality
 
     func testNodesEquality() {
-        let firstNode = Paragraph(text: #"""
+        let text = #"""
         All human beings are born free and equal in dignity and rights.
         They are endowed with reason and conscience
         and should act towards one another in a spirit of brotherhood.
-        """#, replacingNewLinesWithBreaks: true)
-        let secondNode = Paragraph(text: #"""
-        All human beings are born free and equal in dignity and rights.
-        They are endowed with reason and conscience
-        and should act towards one another in a spirit of brotherhood.
-        """#, replacingNewLinesWithBreaks: true)
+        """#
+
+        let firstNode = Paragraph(text: "\(text)", replacingNewLinesWithBreaks: true)
+        let secondNode = Paragraph(text: "\(text)", replacingNewLinesWithBreaks: true)
 
         XCTAssertNotEqual(firstNode, secondNode)
         XCTAssertEqual(firstNode, firstNode)
