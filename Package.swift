f3f0fc21a5c8639a6635bdd47c1058e26e33ae6a
Mattt <mattt@me.com>
2021-05-05 05:18:22 -0700

    Formatting

--- Package.swift
+++ Package.swift
@@ -14,7 +14,8 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/SwiftDocOrg/swift-cmark.git", from: Version(0, 29, 0, prereleaseIdentifiers: [], buildMetadataIdentifiers: ["20210102", "9c8096a"])),
+        .package(url: "https://github.com/SwiftDocOrg/swift-cmark.git",
+                 from: Version(0, 29, 0, buildMetadataIdentifiers: ["20210102", "9c8096a"])),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
